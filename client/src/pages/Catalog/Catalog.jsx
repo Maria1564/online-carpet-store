57c181a85be2627f50c9edb262da9cf7dfeb15fd
Maria <salyginamaria156@gmail.com>
2024-05-11 03:46:28 +0300

    add: поиск товаров (каталог)

--- client/src/pages/Catalog/Catalog.jsx
+++ client/src/pages/Catalog/Catalog.jsx
@@ -5,16 +5,16 @@ import axios from '../../axios'
 import {useDispatch, useSelector } from "react-redux";
 import {  getFavorites } from "../../redux/slices/favorite";
 import { getAllCart} from "../../redux/slices/cart";
-
-
 import Card from "./Card/Card";
 
 const Catalog = () => {
 
     const [products,setProducts] = useState([])
     const [sizes, setSizes] = useState([])
+    const [searchQuery,setSearchQuery] = useState("")
+    const [searchProducts, setSearchProducts] = useState(null)
     const dispatch = useDispatch()
-
+    
     const favorites = useSelector(state => state.favorites.favoriteProducts);
     const cartProducts = useSelector(state => state.cart.products);
     const cartError = useSelector(state => state.cart.isError);
@@ -37,13 +37,30 @@ const Catalog = () => {
       dispatch(getAllCart());
   }, [dispatch]);
   
+
+  const onSearchProducts= ()=>{
+    if(searchQuery.trim() === ""){
+      setSearchProducts(null)
+    }else{
+      let filteredProducts = products.filter(product=>console.log(product.nameproduct.toLowerCase(), searchQuery.trim().toLowerCase(), product.nameproduct.toLowerCase().includes(searchQuery.trim().toLowerCase()))||product.nameproduct.toLowerCase().includes(searchQuery.trim().toLowerCase()))
+      console.log(filteredProducts)
+
+      setSearchProducts(filteredProducts)
+      
+    } 
+
+  }
   return (
     <>
       <Wrapper text="Каталог" />
       <section>
         <div className="container">
+          <div className={s.search}>
+            <input type="text" className={s.inp_query} value={searchQuery} onChange={(e)=> setSearchQuery(e.target.value)}/>
+            <button className={s.btn} onClick={onSearchProducts}>Найти</button>
+          </div>
           <div className={s.cards}>
-            {(!products.length ? <h2>Loading...</h2> : products.map(item => <Card key={item.id} item={item} sizes={sizes} favorites={favorites} cartProducts={cartProducts}/>))}
+            {(Array.isArray(searchProducts) ? searchProducts : products).map(item => <Card key={item.id} item={item} sizes={sizes} favorites={favorites} cartProducts={cartProducts}/>)}
           </div>
         </div>
       </section>
