4aba43bf7cb400f52b2dd60a33f295afe675d93f
Maria <salyginamaria156@gmail.com>
2024-06-10 16:36:40 +0300

    fix: история заказов

--- client/src/pages/ListOrders/ItemOrder/ItemOrder.jsx
+++ client/src/pages/ListOrders/ItemOrder/ItemOrder.jsx
@@ -4,8 +4,13 @@ import s from "./ItemOrder.module.css"
 const ItemOrder = ({orders, idOrder}) => {
   return (
     <div  className={s.order}>
+        <div className={s.info}>
         <span className={s.number_order}>Номер заказа: #{idOrder}</span>
-                    
+        
+        <span>Сумма: <b>{orders[idOrder].reduce((acc, item)=>acc +Number( item.price) , 0)}</b>  руб.</span>
+        </div>
+
+
         {orders[idOrder].map((itemOrder, index) => (
                 <div key={index} className={s.item_order}>
                     <div className={s.about}>
@@ -16,7 +21,7 @@ const ItemOrder = ({orders, idOrder}) => {
                             <span>{itemOrder.quantity} шт.</span> 
                         </div>
                     </div>
-                        <p className={s.price}>{itemOrder.sum} руб.</p>
+                        <p className={s.price}>{itemOrder.price} руб.</p>
                 </div>
             ))
         }
--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -7,12 +7,14 @@ const create = async(req, res)=>{
         const currentDate = new Date().toLocaleDateString().split(".").reverse().join("-");
             
         const newOrder = await db.query("INSERT INTO Orders (idUser, total, orderDate) VALUES ($1, $2, $3) returning * ", [req.id, total, currentDate])
-        console.log(newOrder)
+        
         const idCurrentOrder = newOrder.rows[0].id
 
         //добавление товаров из корзины пользователя в список товаров заказа
-        await db.query(`INSERT INTO orderItems (idOrder, idProduct, idSize, quantity)
-            SELECT $1, idProduct, idSize, quantity FROM Cart WHERE idUser = $2 `, [idCurrentOrder, req.id])
+        await db.query(`INSERT INTO orderItems (idOrder, idProduct, idSize, price, quantity)
+             SELECT $1, idProduct, idSize, SUM(Sizes.price * quantity), quantity FROM Cart, Sizes
+			WHERE Cart.idSize = Sizes.id and idUser = $2
+			GROUP BY  idProduct, idSize, quantity `, [idCurrentOrder, req.id])
 
         res.json({...newOrder.rows[0], orderdate:newOrder.rows[0].orderdate.toLocaleDateString().split(".").reverse().join("-")})
         
@@ -28,10 +30,9 @@ const create = async(req, res)=>{
 const getHistoryOrders = async(req, res) => {
     try{
         const listOrders = await db.query(`
-            SELECT idOrder,  pr.nameProduct, s.name as sizeName, oi.quantity, SUM(oi.quantity * s.price)
+            SELECT idOrder,  pr.nameProduct, s.name as sizeName, oi.quantity, oi.price
             FROM Orders o, orderItems oi, users u, products pr, sizes s
-            where o.iduser = u.id and oi.idProduct = pr.id and oi.idSize = s.id and oi.idOrder = o.id and  o.idUser = $1
-            GROUP BY oi.idOrder,  pr.nameProduct, s.name, oi.quantity ORDER BY oi.idOrder`, [req.id])
+            WHERE o.iduser = u.id and oi.idProduct = pr.id and oi.idSize = s.id and oi.idOrder = o.id and  o.idUser =$1`, [req.id])
 
             res.json(listOrders.rows)
     }catch(err){
